6c756a400acdd0a5a820de2dba3286f15a151391
Serg Puhoff <github@puhoff.com>
2013-06-27 10:09:49 +0400

    ibuffer grouping

--- personal/ibuffer-filter.el
+++ personal/ibuffer-filter.el
@@ -3,9 +3,14 @@
         ("Twig" (filename . ".twig"))
         ("Controllers" (filename . "Controller.php"))
         ("Repositories" (filename . "Repository.php"))
+        ("emacs" (or
+                  (name . "^\\*scratch\\*$")
+                  (name . "^\\*Messages\\*$")))
+        ("Org" (or (mode . org-mode)
+                   (filename . ".org")))
        )))
 
-(add-hook 'ibuffer-mode-hook 
+(add-hook 'ibuffer-mode-hook
   '(lambda ()
      (setq ibuffer-show-empty-filter-groups nil)
      (ibuffer-switch-to-saved-filter-groups "home")))
